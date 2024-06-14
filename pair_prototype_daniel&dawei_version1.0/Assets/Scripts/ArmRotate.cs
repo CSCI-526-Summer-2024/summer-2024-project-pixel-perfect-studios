c6af44798a79cdbe5ffc80e4faadbd077163dd70
Dawei Zhong <daweiz@usc.edu>
2024-06-14 15:09:13 -0700

    fix the truncated trajectory line
    1. Change the input of Raycast and fix the truncated trajectory line.

--- pair_prototype_daniel&dawei_version1.0/Assets/Scripts/ArmRotate.cs
+++ pair_prototype_daniel&dawei_version1.0/Assets/Scripts/ArmRotate.cs
@@ -4,25 +4,13 @@ using UnityEngine;
 
 public class ArmRotate : MonoBehaviour
 {
-    Camera cam;
-    Vector2 MousePos
-    {
-        get
-        {
-            Vector2 Pos = cam.ScreenToWorldPoint(Input.mousePosition);
-            return Pos;
-        }
-    }
-    // Start is called before the first frame update
-    void Awake()
-    {
-        cam = Camera.main;
-    }
+    public Transform shootPoint; 
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 dir = MousePos - (Vector2)transform.position;
+        //Vector2 dir = MousePos - (Vector2)transform.position;
+        Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - shootPoint.position;
         float angle = (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg);
         transform.eulerAngles = new Vector3(0f, 0f, angle);
     }
