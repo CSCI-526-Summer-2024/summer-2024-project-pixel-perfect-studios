8ce1e84a920a8b09da8d480ba341b56cd88701a4
DurexRescue <siqi1000@vip.qq.com>
2024-07-02 11:02:13 -0700

    Change Main Menu
    Bug founded: portal not functioning

--- pair_prototype_daniel&dawei_version1.0/Assets/Scripts/UIController/StartMenu.cs
+++ pair_prototype_daniel&dawei_version1.0/Assets/Scripts/UIController/StartMenu.cs
@@ -7,7 +7,15 @@ public class StartMenu : MonoBehaviour
 {
     public void ClickPlayButton ()
     {
+        SceneManager.LoadScene("Tutorial Level 1");
+    }
+    public void ClickMenuButton ()
+    {
         SceneManager.LoadScene("Level Select");
+    }
+    public void ClickControlButton ()
+    {
+        //SceneManager.LoadScene("Level Select");
     }
 
 }
